535dd16073826da63e127e43d257a0a005bde1ed
a-rvid <git@rvid.eu>
2026-05-08 22:14:16 +0200

    Add random choice of type id and remove test vector.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod startup;
 use crate::startup::startup;
+use rand;
 
 mod types;
-use crate::types::CONTENT_BALL;
 
 fn main() {
     startup();
-    println!("{:?}", CONTENT_BALL);
-    CONTENT_BALL.invoke(Vec::from([1, 2]));
+    let id: usize = rand::random_range(0..2);
+    crate::types::call_id(id);
     ()
 }
--- src/types.rs
+++ src/types.rs
@@ -1,56 +1,42 @@
+mod ball;
+mod cube;
+
 /// This struct defines the type of content, and describes its properties. Example: A ball bouncing physics simulation
 #[derive(Debug)]
 pub struct ContentType {
-    /// Should be incremented
-    id: u32,
     /// Describes the name of the content. Can be used for file names and such.
     name: &'static str,
     /// Describes the purpose of the type. Something like this shall be included in description/title of video.
     description: &'static str,
     /// Pointer to the function to be called. Arguments and return type subject to change.
-    pub call: fn(&ContentType, Vec<u32>) -> (),
+    call: fn(&ContentType) -> (),
 }
 
 impl ContentType {
-    pub fn invoke (&self, v: Vec<u32>) {
-        (self.call)(self, v)
+    pub fn invoke (&self) {
+        (self.call)(self)
     }
-}
-
-
-// Get rid of these println! macros. They are only here for base implementation structure.
-/// Bouncing ball
-fn ball_bounce(content: &ContentType, test: Vec<u32>) -> () {
-    println!("simulation name: {}", content.name);
-    println!("simulation description: {}", content.description);
-    println!("Provided vector: {:#?}", test);
-    // <add ball bounce related stuff here>
-    ()
-}
-
-/// Music plays with ball
-fn cube_music(content: &ContentType, test: Vec<u32>) -> () {
-    println!("simulation name: {}", content.name);
-    println!("simulation description: {}", content.description);
-    println!("Provided vector: {:#?}", test);
-    // <add cube related stuff here>
-    ()
 }
 
 // For these content options, we need to change name and description to something less "formal"
 
 /// Ball bouncing 
-pub const CONTENT_BALL: ContentType = ContentType {
-    id: 0,
+pub const TYPE_BALL: ContentType = ContentType {
     name: "Ball",
     description: "Physics simulation where ball bounces",
-    call: ball_bounce,
+    call: crate::types::ball::run,
 };
 
 /// Cube music
-pub const CONTENT_CUBE_MUSIC: ContentType = ContentType {
-    id: 1,
+const TYPE_CUBE_MUSIC: ContentType = ContentType {
     name: "Cube music",
     description: "A bouncing cube playing music",
-    call: cube_music,
+    call: crate::types::cube::run,
 };
+
+const TYPES: &[ContentType] = &[TYPE_BALL, TYPE_CUBE_MUSIC];
+
+pub fn call_id(id: usize) {
+    TYPES[id].invoke();
+    ()
+}
