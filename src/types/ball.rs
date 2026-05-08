e549e587a43861e0e8ed7c874ab0dc5d13377f45
a-rvid <git@rvid.eu>
2026-05-08 22:27:09 +0200

    Qube and ball (forgot to commit..)

---
+++ src/types/ball.rs
@@ -0,0 +1,10 @@
+use crate::types::ContentType;
+
+// Get rid of these println! macros. They are only here for base implementation structure.
+/// Bouncing ball
+pub fn run(content: &ContentType) -> () {
+    println!("simulation name: {}", content.name);
+    println!("simulation description: {}", content.description);
+    // <add ball bounce related stuff here>
+    ()
+}
