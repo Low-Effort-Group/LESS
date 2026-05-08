081618a21133ef39ce63a8f37d384af5356c1f44
a-rvid <git@rvid.eu>
2026-05-08 20:18:05 +0200

    Add Nix flake I added a nix flake for development, you can run it with nix develop

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "LESS development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "devshell";
+
+          buildInputs = with pkgs; [
+            gcc
+            pkg-config
+            git
+            rustc
+            rustfmt
+            cargo
+            clippy
+          ];
+
+          # shellHook = ''
+          # '';
+        };
+      }
+    );
+}
