612554dc741206ba8c0bdd77f5972239a4b98d69
a-rvid <git@rvid.eu>
2026-05-10 08:41:41 +0200

    Remove startup.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ mod simulation;
 
 use log::*;
 
+use pretty_env_logger::*;
+use log::*;
+
 fn main() {
-    startup();
+    pretty_env_logger::formatted_builder()
+        .parse_filters(&std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string()))
+        .init();
+    info!("Low Effort Slop System initialization...");
     crate::video::setup_encoder();
 }
