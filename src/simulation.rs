970650c5c16176c011984d3e57fd6df8bc96eac8
stoneray <alvar.sahlholm@gmail.com>
2026-05-10 07:58:19 +0200

    removed commented second ball code

--- src/simulation.rs
+++ src/simulation.rs
@@ -7,27 +7,17 @@ use hsl::HSL;
 
 pub fn setupSimulation() -> (Vec<Ball>, Vec<Circle>) {
     let mut ball1 = Ball {
-        x: WIDTH as f32 / 2.0,
+        x: WIDTH as f32 / 2.0 + 100.0,
         y: HEIGHT as f32 / 2.0,
         vx: 200.0, // pixels per second
         vy: 150.0, // pixels per second
         radius: 30.0,
         gravity: 0.0, // pixels per second squared
-        restitution: 1.0, // bounciness self.x = WIDTH as f32 - self.radius;
+        restitution: 1.05, // bounciness self.x = WIDTH as f32 - self.radius;
         friction: 0.8, // friction on bounce
         color: HSL { h: 240.0, s: 1.0, l: 0.5 },
     };
-    // let mut ball2 = Ball {
-    //     x: WIDTH as f32 / 3.0,
-    //     y: HEIGHT as f32 / 3.0,
-    //     vx: -150.0, // pixels per second
-    //     vy: 100.0, // pixels per second
-    //     radius: 20.0,
-    //     gravity: 0.0, // pixels per second squared
-    //     restitution: 1.1, // bounciness
-    //     friction: 0.8, // friction on bounce
-    //     color: HSL { h: 0.0, s: 1.0, l: 0.5 },
-    // };
+
     let mut circle = Circle {
             x: WIDTH as f32 / 2.0,
             y: HEIGHT as f32 / 2.0,
@@ -50,7 +40,7 @@ pub fn newFrame(balls: &mut Vec<Ball>, mut colliders: &mut Vec<Circle>) -> RgbaI
 
     //draw colliders
     colliders.iter_mut().for_each(|collider| {
-        collider.radius -= 1.0; // Shrink the collider
+        collider.radius -= 0.5; // Shrink the collider
         collider.draw(&mut img);
     });
 
