c0202f08ca0510cfb150b867883c0105f19b280b
a-rvid <git@rvid.eu>
2026-05-10 07:04:17 +0200

    set up logger

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ mod video;
 mod types;
 mod simulation;
 
+use log::*;
+
 fn main() {
     startup();
+    info!("Starting simulation...");
     crate::video::setup_encoder();
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,16 @@
+use pretty_env_logger::*;
+use log::*;
+
 // This part of the program initializes and checks all before starting, and defines configuration variables.
 
 /// Initializes the program.
 /// This reads and returns all configuration.
 pub fn startup() {
-    println!("Low Effort Slop System initialization...");
+    // pretty_env_logger::formatted_timed_builder()
+    //     .filter_level(log::LevelFilter::Info)
+    //     .init();
+    pretty_env_logger::formatted_builder()
+        .filter_level(log::LevelFilter::Info)
+        .init();
+    info!("Low Effort Slop System initialization...");
 }
