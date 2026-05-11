c8f9eb0f2f6075325d3eeb326930ca7da6a26d07
a-rvid <git@rvid.eu>
2026-05-11 19:31:11 +0200

    fix balls; TODO: Implement colliders

--- src/simulation.rs
+++ src/simulation.rs
@@ -7,19 +7,7 @@ use crate::types::HSL;
 
 use crate::audio::Audio;
 
-pub fn setupSimulation() -> (Vec<Ball>, Vec<Circle>) {
-    let ball1 = Ball {
-        x: CONFIG.width as f32 / 2.0 + 100.0,
-        y: CONFIG.height as f32 / 2.0,
-        vx: 200.0, // pixels per second
-        vy: 150.0, // pixels per second
-        radius: 30.0,
-        gravity: 0.0, // pixels per second squared
-        restitution: 1.05, // bounciness self.x = CONFIG.width as f32 - self.radius;
-        friction: 0.8, // friction on bounce
-        color: HSL { h: 240.0, s: 1.0, l: 0.5 },
-    };
-
+pub fn setupSimulation() -> Vec<Circle> {//(Vec<Ball>, Vec<Circle>) {
     let circle = Circle {
             x: CONFIG.width as f32 / 2.0,
             y: CONFIG.height as f32 / 2.0,
@@ -41,9 +29,9 @@ pub fn setupSimulation() -> (Vec<Ball>, Vec<Circle>) {
         circle2
     ];
 
-    let dt = 1.0 / 60.0; // simulate at 60 FPS
-    let balls = vec![ball1];
-    (balls, colliders)
+    colliders
+    // let balls = vec![ball1];
+    // (balls, colliders)
 }
 
 pub fn newFrame(balls: &mut Vec<Ball>, mut colliders: &mut Vec<Circle>, frame: &u32, mut sound: &mut Audio) -> RgbaImage {
